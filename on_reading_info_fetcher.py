8e09eccea641cd4c43d59f79f7abf8be6b0c924d
onepinetree <148179315+onepinetree@users.noreply.github.com>
2024-10-28 14:59:53 +0900

    Update on_reading_info_fetcher.py

--- on_reading_info_fetcher.py
+++ on_reading_info_fetcher.py
@@ -8,15 +8,13 @@ class OnReadingInfoFetcher(NotionInfoFetcher):
 
 
     def getOnReadingBookDict(self) -> dict:
-        '''읽고 있는 책의 pageID와 이름을 각각 key와 value로 갖고 있는 dict을 return 한다. dict이 비어있으면 ''을 return 한다.'''
+        '''읽고 있는 책의 pageID와 이름을 각각 key와 value로 갖고 있는 dict을 return 한다.'''
         onreading_dict = {}
         for book_json in self.book_database_json:
             read_status = book_json.get('properties', '').get('독서 상태', '').get('status', '').get('name', '')
             if read_status == '읽는 중':
                 book_name = self.getBookName(book_json=book_json)
                 onreading_dict[book_name] = self.book_page_id_dict.get(book_name)
-        if onreading_dict == {}:
-            return ''
         return onreading_dict
 
 
